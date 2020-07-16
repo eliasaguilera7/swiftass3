deb320ecf862a279b944cba6051d0f3d2d213724
Elias Aguilera Yambay <200393648@ba-a251-2317.student.georgianc.on.ca>
2020-07-16 18:57:24 -0400

    second commit ass3

--- Ass3_Elias_Aguilera/ViewController.swift
+++ Ass3_Elias_Aguilera/ViewController.swift
@@ -18,22 +18,48 @@ class ViewController: UIViewController {
        label.frame = CGRect(x: 0, y: 0, width: 200, height: 21)
                                   
         label.textAlignment = .left
-        label.text = "Georgian College Summer Computer Programmer"
         label.textColor = .white
         label.numberOfLines = 5
+        label.text = "Georgian College Summer Computer Programmer"
         label.layer.shadowOpacity = 0.7
         label.layer.shadowRadius = 2
         label.backgroundColor = UIColor.black
         label.font = UIFont(name: "AVENIR", size: 20)
-        
+
         return label
         
     }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
       myView.addSubview(firstLabel)
+        
+        
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        myView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        myView.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        
+      /*  myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        myView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        myView.leadingAnchor.constraint(equalTo: myView.leadingAnchor).isActive = true
+        myView.trailingAnchor.constraint(equalTo: myView.trailingAnchor).isActive = true*/
+    
+        
     }
 
 
